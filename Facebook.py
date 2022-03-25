d29942066ce5f8eb1036f6f339aa5fb389e5fcf5
ODA946 <102269969+ODA946@users.noreply.github.com>
2022-03-24 22:48:45 -0400

    Add files via upload

---
+++ Facebook.py
@@ -0,0 +1,6 @@
+userName = input ("Hello! Please enter your facebook username!")
+userPassword = input ("Hello! Please enter your facebook password!")
+if(userName.lower() == "leon" and userPassword == "ProgrammingRocks!"): print ("Welcome to Facebook!")
+else:
+    print("Wrong credentials, try again!")
+    
---
+++ helloWorld.py
@@ -0,0 +1,6 @@
+print ("Hello World")
+print ("Hello World, my name is Leon Freeman and I love Python!")
+
+       
+userName = input (" Hello! Please enter your name!")
+print ("Hello", username, "nice to meet you")
